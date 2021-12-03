4726d803143f17ee5bfa1bafb915e43eaeb60047
1 <1@DESKTOP-JVUC12S>
2021-12-03 22:20:08 +0500

    Задание 1,2,3

--- Задание4.cs
+++ Задание4.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ConnectDB;
+using MySql.Data.MySqlClient;
 
 namespace IS_2_19_KalinichenkoPV
 {
@@ -16,5 +18,53 @@ namespace IS_2_19_KalinichenkoPV
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Задание4_Load(object sender, EventArgs e)
+        {
+            Chert conn = new Chert();
+            MySqlConnection connn = new MySqlConnection(conn.chertila);
+            string sql = $"SELECT idStud, fioStud, drStud FROM t_datetime";
+            try
+            {
+                connn.Open();
+                MessageBox.Show("Подключение");
+                MySqlDataAdapter IDataAdapter = new MySqlDataAdapter(sql, connn);
+                DataSet dataset = new DataSet();
+                IDataAdapter.Fill(dataset);
+                dataGridView1.DataSource = dataset.Tables[0];
+                connn.Close();
+            }
+            catch (Exception osh)
+            {
+                MessageBox.Show("Произошла ошибка" + osh);
+                connn.Close();
+            }
+        }
+        string id_rows5 = "0";
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (!e.RowIndex.Equals(-1) && !e.ColumnIndex.Equals(-1) && e.Button.Equals(MouseButtons.Left))
+            {
+                dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
+
+                dataGridView1.CurrentRow.Selected = true;
+
+                string index_rows5;
+
+                index_rows5 = dataGridView1.SelectedCells[0].RowIndex.ToString();
+
+                id_rows5 = dataGridView1.Rows[Convert.ToInt32(index_rows5)].Cells[2].Value.ToString();
+                DateTime x = DateTime.Today;
+                DateTime y = Convert.ToDateTime(dataGridView1.Rows[Convert.ToInt32(index_rows5)].Cells[2].Value.ToString());
+                string resultDays = (x - y).ToString();
+                MessageBox.Show("Со дня рождения прошло " + resultDays.Substring(0, resultDays.Length - 9) + " дней");
+            }
+        }
     }
+    
 }
